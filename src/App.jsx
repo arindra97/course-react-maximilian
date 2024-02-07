eb80426839ea3db9ac722d738b6abdd9568acb34
Riko <muhammad.arindra>
2024-02-07 08:22:50 +0700

    React Event Handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 
 function App() {
+  function handleClick(selectedButton) {
+    console.log(selectedButton);
+}
+
   return (
     <div>
       <Header />
@@ -19,6 +24,16 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]}/>
             <CoreConcept {...CORE_CONCEPTS[3]}/>
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onClick={() => handleClick('components')}>Components</TabButton>
+            <TabButton onClick={() => handleClick('jsx')}>JSX</TabButton>
+            <TabButton onClick={() => handleClick('props')}>Props</TabButton>
+            <TabButton onClick={() => handleClick('state')}>State</TabButton>
+          </menu>
+          Dynamic Content
         </section>
       </main>
     </div>
